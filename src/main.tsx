7f326a57478d1a79da22006a9aa59fa8c92cf838
Nathas the legend <hi@thatoneguy.vip>
2025-12-29 21:08:22 +0530

    feat: integrate PostHog provider for React analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { PostHogProvider } from 'posthog-js/react'
 import App from './App'
 import './index.css'
 
+const options = {
+    api_host: import.meta.env.VITE_POSTHOG_HOST || 'https://us.i.posthog.com',
+    person_profiles: 'identified_only' as const,
+    capture_pageview: false, // We'll manually capture page views for SPA
+    capture_pageleave: true,
+    autocapture: true,
+}
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <PostHogProvider
+            apiKey={import.meta.env.VITE_POSTHOG_KEY}
+            options={options}
+        >
+            <App />
+        </PostHogProvider>
     </StrictMode>,
 )
